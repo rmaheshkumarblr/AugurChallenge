53116b0ae954e4ae6795415d9185288d17bebc16
rmaheshkumarblr <rmaheshkumarblr@gmail.com>
2016-09-14 21:03:11 -0600

    Serving Static page using Express

---
+++ index.js
@@ -0,0 +1,36 @@
+// Express Middleware -- http://expressjs.com/en/api.html
+var express = require('express');
+var app = express();
+// Package to generate a uniqueID -- https://www.npmjs.com/package/shortid
+var shortid = require('shortid');
+
+// To serve static pages from the server --  https://expressjs.com/en/starter/static-files.html
+app.use('/',express.static('public'));
+
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+//   console.log(shortid.generate());
+// });
+
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+// const http = require('http');
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   console.log(req)
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World\n');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
