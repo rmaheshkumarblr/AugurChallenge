34baadf56863b489c3f62f0adb2bed9c67d0b37f
rmaheshkumarblr <rmaheshkumarblr@gmail.com>
2016-09-15 01:21:08 -0600

    Completed Task 4, checking for edge cases

--- README.md
+++ README.md
@@ -8,8 +8,14 @@ Augur Technical Challenge
 ##### Completed Task - A Unique ID is assigned to a device's browser
 
 ### B. When that browser is restarted and the device hits that page again, the browser has the same ID
+##### Completed Task - Since the Cache is being used, even on restart the same ID would be displayed
+
 ### C. After the browser clears it’s cookies, the browser has the same ID
+##### Completed Task - Since the Cache is being used and cookies are not used, even on restart the same ID would be displayed
+
 ### D. After the browser clears cache, cookies, and all, the browser has the same ID
+##### Completed Task - Using the IP address to map the Unique ID to the IP address (Since Routers exists, It is more like doing at Household level rather than a particular user)
+
 ### E. More than one browser (Chrome, Firefox, Opera, IE, Safari, etc.) on the device share the same ID
 ### F. Repeat tests A-to-E on another device and have it pass all tests
 ### G. Running your software at web scale, no two devices share an ID
--- index.js
+++ index.js
@@ -9,14 +9,52 @@ app.use('/',express.static('public'));
 app.use('/bower_components',express.static('bower_components'));
 
 
+
+var ipToUniqueIDMapping = {}
+
+
 app.get('/getUniqueID', function (req, res) {
-  res.send(shortid.generate());
-  console.log(shortid.generate());
+  // Based on https://www.chromium.org/Home/chromium-security/client-identification-mechanisms
+  // http://www.web-caching.com/mnot_tutorial/how.html#CACHE-CONTROL
+  // HTML5 client-side storage mechanisms - Cached objects
+
+  // console.log(req.ip);
+  // console.log(req.headers);
+  // console.log(req.root);
+
+  if(! ( req.ip in ipToUniqueIDMapping ) )
+  {
+  	ipToUniqueIDMapping[req.ip] = shortid.generate(); 
+  }
+
+  console.log(ipToUniqueIDMapping)
+
+  if (req.headers['if-modified-since']) 
+  {
+  	console.log('Not Modified')
+	return res.status(304).end();
+  }
+
+  // https://stackoverflow.com/questions/1046966/whats-the-difference-between-cache-control-max-age-0-and-no-cache
+  // We want to use cache but we want to check at each instance when the URL is hit. Hence we set max-age=0
+  res.set({
+  	'Cache-Control': 'max-age=0',
+  	'Last-Modified': new Date(),
+  })
+
+  res.status(200).send(ipToUniqueIDMapping[req.ip]);  
+  
+});
+
+app.get('/sendUniqueID/:uniqueID', function(req, res) {
+  var uniqueID = req.params.uniqueID;
+  console.log(uniqueID)
+  return res.status(304).end();
 });
 
 
 
-app.listen(3000, function () {
+app.listen(3000, '0.0.0.0', function () {
   console.log('Example app listening on port 3000!');
 });
 
